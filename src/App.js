73eb0c5cabe6c753d2938cb0048426b48fedfa00
andreavenez <andrea.veneziano@sydea.it>
2024-10-03 18:32:28 +0200

    20241003 button in stile material e input in stile sap

--- src/App.js
+++ src/App.js
@@ -1,44 +1,148 @@
 import Button from './components/input/button/button';
+import Input from './components/input/input/input';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
   return (
     <div style={{ padding: '20px' }}>
-    <h1>Componenti in Stile SAPUI5</h1>
-    <h4>Button normal</h4>
-    {/* Button Component */}
-    <div style={{display:'flex', gap:'20px'}} >
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} />
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="secondary"/>
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="tertiary"/>  
-    <Button label="Click Me" variant="success"/>  
-    <Button label="Click Me" variant="danger"/>  
-    <Button label="Click Me" variant="warning"/>  <br></br>
-  </div>
-  <h4>Button disabled</h4>
-  <div style={{display:'flex', gap:'20px'}} >
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} disabled='true'/>
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="secondary" disabled='true'/>
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="tertiary" disabled='true'/>  
-    <Button label="Click Me" variant="success" disabled='true'/>  
-    <Button label="Click Me" variant="danger" disabled='true'/>  
-    <Button label="Click Me" variant="warning" disabled='true'/>  
-  </div> 
-  <h4>Button focused</h4>
-    {/* Button Component */}
-    <div style={{display:'flex', gap:'20px'}} >
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} />
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="secondary"/>
-    <Button label="Click Me" onClick={() => alert('Button clicked!')} variant="tertiary"/>  
-    <Button label="Click Me" variant="success"/>  
-    <Button label="Click Me" variant="danger"/>  
-    <Button label="Click Me" variant="warning"/>  <br></br>
-  </div>
-    
-    <h4>Input</h4>  
-      
-
+      <h1>Pulsanti in Stile SAPUI5</h1>
+      <h4>Normal</h4>
+      {/* Button Component */}
+      <div style={{display:'flex', gap:'20px'}} >
+      <Button label="Primary" onClick={() => alert('Button clicked!')} />
+      <Button label="Secondary" onClick={() => alert('Button clicked!')} variant="secondary"/>
+      <Button label="Tertiary" onClick={() => alert('Button clicked!')} variant="tertiary"/>  
+      <Button label="Success" variant="success"/>  
+      <Button label="error" variant="error"/>  
+      <Button label="Warning" variant="warning"/>  <br></br>
     </div>
+    <h4>Disabled</h4>
+    <div style={{display:'flex', gap:'20px'}} >
+      <Button label="Primary" onClick={() => alert('Button clicked!')} disabled='true'/>
+      <Button label="Secondary" onClick={() => alert('Button clicked!')} variant="secondary" disabled='true'/>
+      <Button label="Tertiary" onClick={() => alert('Button clicked!')} variant="tertiary" disabled='true'/>  
+      <Button label="Success" variant="success" disabled='true'/>  
+      <Button label="error" variant="error" disabled='true'/>  
+      <Button label="Warning" variant="warning" disabled='true'/>  
+    </div> 
+    {/* <h4>Focused</h4> */}
+      {/* Button Component */}
+      {/* <div style={{display:'flex', gap:'20px'}} >
+      <Button label="Primary" onClick={() => alert('Button clicked!')} />
+      <Button label="Secondary" onClick={() => alert('Button clicked!')} variant="secondary"/>
+      <Button label="Tertiary" onClick={() => alert('Button clicked!')} variant="tertiary"/>  
+      <Button label="Success" variant="success"/>  
+      <Button label="error" variant="error"/>  
+      <Button label="Warning" variant="warning"/>  <br></br>
+    </div> */}
+    
+    <h1>Pulsanti in Stile MaterialUi</h1>
+    <h4>Normal</h4>
+    
+      <div style={{display:'flex', gap:'20px'}} >
+        <Button label="Primary" theme='material' /> 
+        <Button label="Secondary" variant="secondary" theme='material'/>
+        <Button label="Tertiary" variant="tertiary" theme='material'/>
+        <Button label="Success" variant="success" theme='material'/>
+        <Button label="error" variant="error" theme='material'/>
+        <Button label="Warning" variant="warning" theme='material'/>
+      </div> 
+      <h4>Disabled</h4>
+     
+      <div style={{display:'flex', gap:'20px'}} >
+        <Button label="Primary" theme='material' disabled='true' /> 
+        <Button label="Secondary" variant="secondary" disabled='true' theme='material'/>
+        <Button label="Tertiary" variant="tertiary" disabled='true' theme='material'/>
+        <Button label="Success" variant="success" disabled='true' theme='material'/>
+        <Button label="error" variant="error" disabled='true' theme='material'/>
+        <Button label="Warning" variant="warning" disabled='true' theme='material'/>
+      </div> 
+      <br/> 
+      <hr/>
+      <h1>Input field in Stile SAPUI5</h1>
+      <h4>Normal</h4>
+      <Input 
+        label="Required" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your name" 
+        required={true} 
+        theme="sap"
+        variant="default"
+      />
+      <Input 
+        label="Not required" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false} 
+        theme="sap"
+        variant="default"
+      />
+       <Input 
+        label="Disabled" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false}
+        disabled={true}
+        theme="sap"
+        variant="default"
+      />
+       <Input 
+        label="Read Only" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false} 
+        disabled={true}
+        readOnly={true}
+        theme="sap"
+        variant="default"
+      />
+       <Input 
+        label="Error" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false} 
+        theme="sap"
+        variant="error"
+      />
+       <Input 
+        label="Success" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false} 
+        theme="sap"
+        variant="success"
+      />
+      <Input 
+        label="Warning" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false} 
+        theme="sap"
+        variant="warning"
+      />
+      <Input 
+        label="Information" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter your email" 
+        required={false}
+        theme="sap"
+        variant="information"
+      />
+    </div>
+    
   );
 }
 
--- src/components/input/button/button.js
+++ src/components/input/button/button.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './button.css';
 
-const Button = ({ label, onClick, variant = 'default', disabled = false }) => {
-    const buttonClass = `ui5-button ui5-button-${variant} ${disabled ? `ui5-button-${variant}-disabled` : ''}`;
+const Button = ({ label, onClick, variant = 'default', disabled = false ,theme = 'sap' }) => {
+  /*useEffect(() => {
+    const btnMaterialDefault = document.querySelectorAll(".material-button-default");
+    btnMaterialDefault.forEach((btn)=> {
+      btn.addEventListener('click', (e) => {
+        const { pageX, pageY, currentTarget } = e;
+        let x = ((pageX - currentTarget.offsetLeft)*100) / currentTarget.offsetWidth;
+        let y = ((pageY - currentTarget.Top)*100) / currentTarget.offsetHeight;
+        const ripple = document.createElement("span");
+        // const rippleColor = "#c2e0ff00";
+        ripple.classList.add("ripple-effect-default");
+        // ripple.style.background = rippleColor;
+        ripple.style.left = x + "%";
+        ripple.style.top = y + "%";
+        btn.appendChild(ripple);
+        
+        
+        setTimeout(() => {
+          ripple.remove()
+        },1000);
+        
+      });
+    });
+  }, []); */
+  useEffect(() => {
+    if (theme === 'material') {
+      const buttons = document.querySelectorAll(`[class^="material-button"]`);
+
+      const handleRipple = (e, rippleClass) => {
+        const { pageX, pageY, currentTarget } = e;
+        const x = ((pageX - currentTarget.offsetLeft) * 100) / currentTarget.offsetWidth;
+        const y = ((pageY - currentTarget.offsetTop) * 100) / currentTarget.offsetHeight;
+
+        const ripple = document.createElement('span');
+        ripple.classList.add(rippleClass);
+        ripple.style.left = `${x}%`;
+        ripple.style.top = `${y}%`;
+        currentTarget.appendChild(ripple);
+
+        setTimeout(() => {
+          ripple.remove();
+        }, 1000);
+      };
+
+      buttons.forEach((btn) => {
+        let rippleClass;
+        if (btn.classList.contains('material-button-secondary')) {
+          rippleClass = 'ripple-effect-secondary';
+        } else if (btn.classList.contains('material-button-tertiary')) {
+          rippleClass = 'ripple-effect-tertiary';
+        } else if (btn.classList.contains('material-button-success')) {
+          rippleClass = 'ripple-effect-success';
+        } else if (btn.classList.contains('material-button-error')) {
+          rippleClass = 'ripple-effect-error';
+        } else if (btn.classList.contains('material-button-warning')) {
+          rippleClass = 'ripple-effect-warning';
+        }else {
+          rippleClass = 'ripple-effect-default';
+        }
+
+        btn.addEventListener('click', (e) => handleRipple(e, rippleClass));
+      });
+
+      // Cleanup: Rimuovi l'event listener quando il componente si smonta
+      return () => {
+        buttons.forEach((btn) => {
+          btn.removeEventListener('click', handleRipple);
+        });
+      };
+    }
+  }, [theme]);
+  const buttonClass = `${theme}-button ${theme}-button-${variant} ${disabled ? `${theme}-button-${variant}-disabled` : ''}`;
+  
   
   return (
     <button 
